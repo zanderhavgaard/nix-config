ff3c0349245124f381dc8193d425db5bd881ab51
zanderhavgaard <zander@havgaard.dk>
2025-01-06 19:22:46 +0100

    add phobos host

--- flake.nix
+++ flake.nix
@@ -79,6 +79,26 @@
             }
           ];
         };
+        phobos = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          specialArgs = {
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          };
+          modules = [
+            ./hosts/phobos/configuration.nix
+
+            # setup home-manager as a module
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.zander = import ./hosts/phobos/home.nix;
+            }
+          ];
+        };
       };
     };
 }
---
+++ hosts/phobos/home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+let
+  username = "zander";
+  homePath = "/home/${username}";
+  configPath = "${homePath}/nix-config/config";
+in
+{
+  imports = [
+    # ../../modules/waybar/waybar.nix
+    # ../../modules/river.nix
+    ../../modules/wezterm.nix
+    ../../modules/fish.nix
+    ../../modules/home-manager/git.nix
+    ../../modules/home-manager/zsh.nix
+  ];
+
+  # Configure the user
+  home.username = username;
+  home.homeDirectory = homePath;
+
+  # define the nixpkgs version to use
+  home.stateVersion = "24.11";
+
+  # let home-manager install and mange itself
+  programs.home-manager.enable = true;
+
+  # packages installed for the user
+  home.packages = with pkgs; [
+  ];
+
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      auto_update = true;
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+    settings = {
+      theme = "one-half-dark";
+    };
+    # enableZshIntegration = true;
+  };
+}
