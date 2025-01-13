51dd447da4ccefbc471a7c5a389411212e27eb77
zanderhavgaard <zander@havgaard.dk>
2025-01-13 10:21:03 +0100

    add config to fish module

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -1,66 +1,60 @@
 { config, pkgs, ... }:
 {
-  # programs.direnv = {
-  #   enable = true;
-  #   enableFishIntegration = true;
-  # };
-  # programs.zoxide = {
-  #   enable = true;
-  #   enableFishIntegration = true;
-  # };
-  #
-  # programs.fzf = {
-  #   enable = true;
-  #   enableFishIntegration = true;
-  # };
-  #
-  # programs.yazi = {
-  #   enableFishIntegration = true;
-  # };
-  # home.packages = with pkgs; [
-  #   fish
-  #   starship
-  # ];
+  programs.direnv = {
+    enable = true;
+    # throws error?
+    # enableFishIntegration = true;
+  };
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
 
-  # programs.starship = {
-  #   enable = true;
-  #   enableFishIntegration = true;
-  # };
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
 
-  # programs.fish.enable = true;
+  programs.yazi = {
+    enableFishIntegration = true;
+  };
 
-  # programs.fish = {
-  #   enable = true;
-  # these seem to be installed but not loaded ...
-  # plugins = [
-  #   {
-  #     name = "autopair";
-  #     src = pkgs.fishPlugins.autopair.src;
-  # }
-  #   {
-  #     name = "done";
-  #     src = pkgs.fishPlugins.done.src;
-  #   }
-  #   {
-  #     name = "fish-you-should-use";
-  #     src = pkgs.fishPlugins.fish-you-should-use.src;
-  #   }
-  #   {
-  #     name = "colored-man-pages";
-  #     src = pkgs.fishPlugins.colored-man-pages.src;
-  #   }
-  #   {
-  #     name = "plugin-git";
-  #     src = pkgs.fishPlugins.plugin-git.src;
-  #   }
-  #   {
-  #     name = "grc";
-  #     src = pkgs.fishPlugins.grc.src;
-  #   }
-  #   {
-  #     name = "fish-bd";
-  #     src = pkgs.fishPlugins.fish-bd.src;
-  #   }
-  #   ];
-  # };
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "autopair";
+        src = pkgs.fishPlugins.autopair.src;
+      }
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
+      {
+        name = "fish-you-should-use";
+        src = pkgs.fishPlugins.fish-you-should-use.src;
+      }
+      {
+        name = "colored-man-pages";
+        src = pkgs.fishPlugins.colored-man-pages.src;
+      }
+      {
+        name = "plugin-git";
+        src = pkgs.fishPlugins.plugin-git.src;
+      }
+      {
+        name = "grc";
+        src = pkgs.fishPlugins.grc.src;
+      }
+      {
+        name = "fish-bd";
+        src = pkgs.fishPlugins.fish-bd.src;
+      }
+    ];
+  };
 }
