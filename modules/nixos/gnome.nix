38da000d3e09848c4da2d8410c532af30ce804d7
zanderhavgaard <zander@havgaard.dk>
2025-01-19 01:29:10 +0100

    add gnome nixos module

---
+++ modules/nixos/gnome.nix
@@ -0,0 +1,20 @@
+{ ... }:
+{
+  services = {
+    xserver = {
+      enable = true;
+
+      displayManager = {
+        gdm = {
+          enable = true;
+        };
+      };
+
+      desktopManager = {
+        gnome = {
+          enable = true;
+        };
+      };
+    };
+  };
+}
