0b068976cc2599060beea2570eff4dff0677bb65
zanderhavgaard <zander@havgaard.dk>
2025-01-19 01:30:16 +0100

    remove hardware stuff from steam module

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -2,33 +2,30 @@
 # https://nixos.wiki/wiki/Steam
 # https://journix.dev/posts/gaming-on-nixos/
 # https://github.com/fufexan/nix-gaming
+#
+# NOTE: this file is currently very specific to venera
+#
 { pkgs, ... }:
 {
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-
-    nvidia.modesetting.enable = true;
-  };
-
-  # These should be set through nixos hardware flake
-  # even though it says xserver, should enable for both x and wayland
-  # services.xserver.videoDrivers = [“nvidia”];
-  # services.xserver.videoDrivers = [“amdgpu”];
 
   programs = {
     steam = {
       enable = true;
 
-      remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-      localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
-
       gamescopeSession = {
         enable = true;
       };
+
+      remotePlay = {
+        openFirewall = false; # Open ports in the firewall for Steam Remote Play
+      };
+      dedicatedServer = {
+        openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+      };
+      localNetworkGameTransfers = {
+        openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
+      };
+
     };
 
     gamemode = {
@@ -39,5 +36,4 @@
   environment.systemPackages = with pkgs; [
     mangohud
   ];
-
 }
