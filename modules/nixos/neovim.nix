78fee94d0e68925dc36b9dc2cba777b15202c680
zanderhavgaard <zander@havgaard.dk>
2025-02-02 19:14:56 +0100

    wip on using system binaries for lsp/linters/formatters used by neovim

--- modules/nixos/neovim.nix
+++ modules/nixos/neovim.nix
@@ -19,12 +19,16 @@
 
   # neovim dependencies
   environment.systemPackages = with pkgs; [
+    tree-sitter
     gcc
     lua5_1
     lua51Packages.tiktoken_core
+    # TODO: figure which is the right tiktoken package for neovim
+    # lua
+    # luaPackages.tiktoken_core
+    # luajitPackages.tiktoken_core
     luajit
     luarocks
-    stylua
     lynx
     viu
     chafa
@@ -35,5 +39,37 @@
     lazygit
     ripgrep
     fd
+
+    # WIP: not sure that all of these are picked up correctly by neovim
+    # lsp/linters/etc.
+    bash-language-server
+    black
+    python3Packages.debugpy
+    docker-compose-language-service
+    dockerfile-language-server-nodejs
+    hadolint
+    helm-ls
+    lua-language-server
+    nodePackages.vscode-json-languageserver
+    # markdown-toc # TODO: what is the packge name?
+    markdownlint-cli2 # TODO: this is not recognized
+    marksman
+    nil
+    nixd
+    nixfmt-rfc-style
+    nodePackages.prettier
+    pyright
+    basedpyright
+    ruff
+    shellcheck
+    shfmt
+    sqlfluff
+    stylua
+    taplo
+    terraform
+    opentofu
+    terraform-ls
+    tflint
+    yaml-language-server
   ];
 }
