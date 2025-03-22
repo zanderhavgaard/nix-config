019a614f559aaa23e33aa25ed51849c43920e156
zanderhavgaard <zander@havgaard.dk>
2025-03-22 14:52:16 +0100

    enable roon-bridge on phobos

--- hosts/phobos/configuration.nix
+++ hosts/phobos/configuration.nix
@@ -33,6 +33,12 @@
   # Use neovim from nixpkgs, instead of compiling from source
   # programs.neovim.package = lib.mkForce pkgs.neovim-unwrapped;
 
+  # roon bridge
+  services.roon-bridge = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
