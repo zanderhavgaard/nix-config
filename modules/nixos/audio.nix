edb421f6b47a90194a1a0f3887462d51f55c3372
zanderhavgaard <zander@havgaard.dk>
2025-04-10 10:07:42 +0200

    allow pipewire to play high sample rate audio

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -14,5 +14,17 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+
+    # allow pipewire to play high sample rate audio
+    extraConfig.pipewire.noresample = {
+      "context.properties" = {
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          96000
+          192000
+        ];
+      };
+    };
   };
 }
