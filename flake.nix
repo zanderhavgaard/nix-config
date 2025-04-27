18b2ab4beed04e6d67f3f172ead3b5087bb6a9a0
zanderhavgaard <zander@havgaard.dk>
2025-04-27 23:42:20 +0200

    add nostromo host

--- flake.nix
+++ flake.nix
@@ -35,6 +35,34 @@
     }:
     {
       nixosConfigurations = {
+        nostromo = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          };
+          modules = [
+            ./hosts/nostromo/configuration.nix
+            nixos-hardware.nixosModules.common-cpu-amd
+            nixos-hardware.nixosModules.common-cpu-amd-pstate
+	    # fails to build for some reason ...
+            # nixos-hardware.nixosModules.common-cpu-amd-zenpower
+            nixos-hardware.nixosModules.common-gpu-amd
+            nixos-hardware.nixosModules.common-pc
+            nixos-hardware.nixosModules.common-pc-ssd
+
+            # setup home-manager as a module
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.zander = import ./hosts/nostromo/home.nix;
+            }
+          ];
+        };
         orion = nixpkgs.lib.nixosSystem rec {
           system = "x86_64-linux";
           specialArgs = {
