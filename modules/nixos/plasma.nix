30c45728cd67df0d1d9183aa2c24361e224b823b
zanderhavgaard <zander@havgaard.dk>
2025-04-29 19:46:38 +0200

    try to fix plasma running under wayland

--- modules/nixos/plasma.nix
+++ modules/nixos/plasma.nix
@@ -1,39 +1,43 @@
 # sometimes its conveient to be able to fall back on plasma when things dont work
 { ... }:
 {
-  # services = {
-  #
-  #   xserver = {
-  #     enable = true;
-  #   };
-  #
-  #   displayManager = {
-  #     sddm = {
-  #       enable = true;
-  #       wayland.enable = true;
-  #     };
-  #   };
-  #
-  #   desktopManager = {
-  #     plasma6 = {
-  #       enable = true;
-  #     };
-  #   };
-  #
-  # };
+  services = {
+
+    xserver = {
+      enable = true;
+    };
+
+    displayManager = {
+      sddm = {
+        enable = true;
+
+        wayland = {
+          enable = true;
+        };
+      };
+    };
+
+    desktopManager = {
+      plasma6 = {
+        enable = true;
+        enableQt5Integration = false;
+      };
+    };
+
+  };
 
   # FIXME: for some reason plasma crashes instantly when starting under wayland
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasma";
+  # services.xserver.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+  # services.displayManager.defaultSession = "plasma";
+  #
+  # services.displayManager.sddm.wayland.enable = true;
 
-  services.displayManager.sddm.wayland.enable = true;
-
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
+  # qt = {
+  #   enable = true;
+  #   platformTheme = "gnome";
+  #   style = "adwaita-dark";
+  # };
 
 }
