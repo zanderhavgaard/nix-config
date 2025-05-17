c100e8803875ab364d6ad58a19fe8a1bd7784ef7
zanderhavgaard <zander@havgaard.dk>
2025-05-17 13:23:15 +0200

    try to fix plamsa module, still not working ...

--- modules/nixos/plasma.nix
+++ modules/nixos/plasma.nix
@@ -1,5 +1,7 @@
 # sometimes its conveient to be able to fall back on plasma when things dont work
-{ ... }:
+{
+  ...
+}:
 {
   services = {
 
@@ -8,36 +10,25 @@
     };
 
     displayManager = {
+      # sddm seems to be bugged currently ...
       sddm = {
-        enable = true;
-
-        wayland = {
-          enable = true;
-        };
+        enable = false;
+        wayland.enable = false;
       };
     };
 
     desktopManager = {
       plasma6 = {
         enable = true;
-        enableQt5Integration = false;
       };
     };
 
   };
 
-  # FIXME: for some reason plasma crashes instantly when starting under wayland
-  # services.xserver.enable = true;
-  # services.displayManager.sddm.enable = true;
-  # services.desktopManager.plasma6.enable = true;
-  # services.displayManager.defaultSession = "plasma";
-  #
-  # services.displayManager.sddm.wayland.enable = true;
-
-  # qt = {
-  #   enable = true;
-  #   platformTheme = "gnome";
-  #   style = "adwaita-dark";
-  # };
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
 
 }
