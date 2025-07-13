2125d3ea05c6d01d900b4e5cfa6cd0d447149395
Zander Hornung Havgaard <zander@havgaard.dk>
2025-07-13 23:20:33 +0200

    change nfs root on mothership

--- hosts/mothership/nfs.nix
+++ hosts/mothership/nfs.nix
@@ -6,10 +6,10 @@
     enable = true;
 
     exports = ''
-      /srv/kubernetes-nfs-data \
-           192.168.1.101(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000) \
-           192.168.1.102(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000) \
-           192.168.1.103(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000)
+      /srv \
+        192.168.1.101(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000) \
+        192.168.1.102(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000) \
+        192.168.1.103(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000)
     '';
   };
 
