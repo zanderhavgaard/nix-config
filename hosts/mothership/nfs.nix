2ea35cb2b52a8f78c52498c34b58bbfea348fc52
Zander Hornung Havgaard <zander@havgaard.dk>
2025-07-13 23:41:46 +0200

    update nfs export path

--- hosts/mothership/nfs.nix
+++ hosts/mothership/nfs.nix
@@ -6,7 +6,7 @@
     enable = true;
 
     exports = ''
-      /srv \
+      /srv/kubernetes-nfs-data \
         192.168.1.101(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000) \
         192.168.1.102(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000) \
         192.168.1.103(rw,sync,no_subtree_check,fsid=0,all_squash,anonuid=1000,anongid=1000)
