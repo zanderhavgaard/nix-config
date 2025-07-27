b4cc1a80ae095cdf4df7e9f17b2b003bef4490ee
Zander Hornung Havgaard <zander@havgaard.dk>
2025-07-27 13:04:46 +0200

    update mothership iscsi.nix

--- hosts/mothership/iscsi.nix
+++ hosts/mothership/iscsi.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
@@ -9,4 +10,21 @@
     targetcli
     lvm2
   ];
+
+  # automatically load the target config on boot
+  systemd.services.iscsi-restore = {
+    description = "Restore iSCSI config via targetcli";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.targetcli}/bin/targetcli restoreconfig";
+      Environment = "PATH=${
+        lib.makeBinPath [
+          pkgs.kmod
+          pkgs.coreutils
+        ]
+      }";
+    };
+  };
 }
